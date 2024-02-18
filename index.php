1ac2d5c3608a7c42665f22b7cfb9cbab20ad5163
= <renatomdavid811@hotmail.com>
2024-02-18 15:32:34 -0300

    updgrade

--- index.php
+++ index.php
@@ -15,12 +15,12 @@
 		include ("./php/navbar.php"); ?>
 	</header>
 	<div class="container ">
-  <div class="row  ">
+  <div class="row">
     <div class="col-md-6 ">
       <img src="img/destaque-home.png"  alt="Promoção: Big City Night" style="max-width: 100%; height: auto; margin:20px;">
     </div>
     <div class="col-md-6">
-      <fieldset class="col-md-9">
+      <fieldset class="col-md-10">
         <div class="col-md-11">
           <section class="busca col-md-12">
             <h2 class="col-md-10">Busca</h2>
@@ -31,7 +31,7 @@
           </section>
           <section class="menu-departamentos col-md-12">
             <nav class="navbar-inverse">
-              <h2 >Departamentos</h2>
+              <h2>Departamentos</h2>
               <ul class="departamentos-lista">
                 <li><a href="#">Blusas e Camisas</a>
                   <ul class="sublista">
--- produto.php
+++ produto.php
@@ -21,32 +21,35 @@
 				<p>por apenas R$ 129,00</p>
 
 				<form action="checkout.php" method="$_POST">
-			    	<fieldset class="cores">
-			        	<legend>Escolha a cor</legend>
+					<div class="row col-md-6">
 
-			          	<input type="radio" name="cor" value="verde" id="verde" checked>
-			          	<label for="verde">
-			            	<img src="img/produtos/foto1-verde.png" alt="verde">
-			         	</label>
-			          
-			          	<input type="radio" name="cor" value="rosa" id="rosa">
-			          	<label for="rosa">
-			            	<img src="img/produtos/foto1-rosa.png" alt="rosa">
-			          	</label>
-			          
-			          	<input type="radio" name="cor" value="azul" id="azul">
-			          	<label for="azul">
-			            	<img src="img/produtos/foto1-azul.png" alt="azul">
-			          	</label>
-			      	</fieldset>
+						<fieldset class="cores">
+							<legend>Escolha a cor</legend>
 
+							<input type="radio" name="cor" value="verde" id="verde" checked>
+							<label for="verde">
+								<img src="img/produtos/foto1-verde.png" alt="verde">
+							</label>
+						
+							<input type="radio" name="cor" value="rosa" id="rosa">
+							<label for="rosa">
+								<img src="img/produtos/foto1-rosa.png" alt="rosa">
+							</label>
+						
+							<input type="radio" name="cor" value="azul" id="azul">
+							<label for="azul">
+								<img src="img/produtos/foto1-azul.png" alt="azul">
+							</label>
+						</fieldset>
+					 </div>
+					 <div class="row col-md-6">
 			      	<fieldset class="tamanhos">
 						<legend>Escolha o tamanho:</legend>
 
 						<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho" oninput="output.value = this.value">
 					 	<output name="output" for="tamanho" value="42">42
 				  	</fieldset>
-
+					</div>
 			      	<input type="submit" class="comprar" value="Comprar">
 			  	</form>
 			</div>
